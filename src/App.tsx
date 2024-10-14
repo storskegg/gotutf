2604ee776b0034152938c8abdadfa1265d487abd
Liam Conrad <liam@w4pho.net>
2024-10-14 10:33:48 -0500

    stash

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import Navbar from "./components/Navbar";
 import Custom from "./components/Custom";
 import "./custom.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Custom />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar />
+//         <Custom />
+//       </div>
+//     );
+//   }
+// }
+
+const App: React.SFC = () => <div><Navbar/><Custom/></div>
 
 export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+// import "semantic-ui-css/semantic.min.css"; // optional
+import App from "./App";
+
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.render(<App />, root);
